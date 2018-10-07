85c3caf653b71981ee9bad675c301c6c5eef6cd3
Sam Gardner <samcgardner@gmail.com>
2018-10-07 11:10:52 +0100

    Properly depend on base and use a converging value for alpha in demo

--- src/Main.hs
+++ src/Main.hs
@@ -6,7 +6,7 @@ import Types
 main :: IO ()
 main = do
   let thetas = Coefficients (0, 0)
-  let alpha = 1
+  let alpha = 0.3
   let trainingset =
         TrainingSet [Example (0, 50), Example (1, 60), Example (2, 70)]
   let iterations = 500
